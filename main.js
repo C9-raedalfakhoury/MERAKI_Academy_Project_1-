311865164dd8b6f736fa7de8e63caa5371a8160f
raedalfakhoury <alfakhouryraed@gmail.com>
2023-11-12 20:37:07 +0300

    end logic 65%

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const images = [
-  { id: 1, src: "./image/firebase.jpg", alt: "./front/front.jpg" },
-  { id: 1, src: "./image/firebase.jpg", alt: "./front/front.jpg" },
-  { id: 2, src: "./image/flutter.jpg", alt: "./front/front.jpg" },
-  { id: 2, src: "./image/flutter.jpg", alt: "./front/front.jpg" },
-  { id: 3, src: "./image/mongoDB.jpg", alt: "./front/front.jpg" },
-  { id: 3, src: "./image/mongoDB.jpg", alt: "./front/front.jpg" },
-  { id: 4, src: "./image/nodejs.jpg", alt: "./front/front.jpg" },
-  { id: 4, src: "./image/nodejs.jpg", alt: "./front/front.jpg" },
-  { id: 5, src: "./image/postgress.jpg", alt: "./front/front.jpg" },
-  { id: 5, src: "./image/postgress.jpg", alt: "./front/front.jpg" },
-  { id: 6, src: "./image/react.jpg", alt: "./front/front.jpg" },
-  { id: 6, src: "./image/react.jpg", alt: "./front/front.jpg" },
-  { id: 7, src: "./image/redux.jpg", alt: "./front/front.jpg" },
-  { id: 7, src: "./image/redux.jpg", alt: "./front/front.jpg" },
-  { id: 8, src: "./image/tailwind.jpg", alt: "./front/front.jpg" },
-  { id: 8, src: "./image/tailwind.jpg", alt: "./front/front.jpg" },
+  { id: 1, src: "./image/firebase.jpg", alt: "./front/front.png" },
+  { id: 2, src: "./image/flutter.jpg", alt: "./front/front.png" },
+  { id: 2, src: "./image/flutter.jpg", alt: "./front/front.png" },
+  { id: 3, src: "./image/mongoDB.jpg", alt: "./front/front.png" },
+  { id: 1, src: "./image/firebase.jpg", alt: "./front/front.png" },
+  { id: 6, src: "./image/react.jpg", alt: "./front/front.png" },
+  { id: 4, src: "./image/nodejs.jpg", alt: "./front/front.png" },
+  { id: 4, src: "./image/nodejs.jpg", alt: "./front/front.png" },
+  { id: 5, src: "./image/postgress.jpg", alt: "./front/front.png" },
+  { id: 5, src: "./image/postgress.jpg", alt: "./front/front.png" },
+  { id: 6, src: "./image/react.jpg", alt: "./front/front.png" },
+  { id: 3, src: "./image/mongoDB.jpg", alt: "./front/front.png" },
+  { id: 8, src: "./image/tailwind.jpg", alt: "./front/front.png" },
+  { id: 7, src: "./image/redux.jpg", alt: "./front/front.png" },
+  { id: 7, src: "./image/redux.jpg", alt: "./front/front.png" },
+  { id: 8, src: "./image/tailwind.jpg", alt: "./front/front.png" },
 ];
 function shuffleArray(images) {
   for (let i = images.length - 1; i > 0; i--) {
@@ -102,15 +102,17 @@ const playerName = document.createElement("div");
 playerName.className = "playerName";
 let count = 1;
 let id;
+
 const renderImage = () => {
   images.forEach((image, index) => {
     const card = document.createElement("div");
     card.className = "card";
     const img = document.createElement("img");
     img.className = "img";
-    img.src = image.src;
+    img.src = image.alt;
     img.id = image.id;
-    img.setAttribute("index", index);
+
+    const imageId = document.querySelector(".img");
     card.append(img);
     mainDivCard.append(card);
 
@@ -119,25 +121,35 @@ const renderImage = () => {
     img.addEventListener("click", (e) => {
       if (count === 1) {
         id = e.target.id;
+        img.src = image.src;
+        //  return ;
       }
       if (count === 2) {
         img.src = image.src;
+        const firstImage = document.querySelector(`#${id}`);
+
+        const secondImage = document.querySelector(`#${e.target.id}`);
+
         if (id === e.target.id) {
           console.log("yes");
           console.log(`ID is ${id} TARGET ${e.target.id}`);
-          // img.src = image.src;
+
+          console.log(`f : ${firstImage}`);
+
+          console.log(`s : ${secondImage}`);
         } else {
           console.log("no");
           console.log(`ID is ${id} TARGET ${e.target.id}`);
+          img.src = image.src;
         }
-        count = 0;
+        count = 1;
       }
       count++;
     });
   });
 };
 
-buttonPalyNow.addEventListener("click", () => {
+buttonPalyNow.addEventListener("click", (e) => {
   renderImage();
   playerName.innerText = `Player Name : ${inputUserName.value}`;
 
